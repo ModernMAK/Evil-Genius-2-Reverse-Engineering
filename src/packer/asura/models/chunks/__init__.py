f51fe0e65d320e3190028b37b057de3684cdf576
Marcus Kertesz <ModernMAK@Hotmail.com>
2021-05-03 18:02:15 -0700

    Moved non-generic chunks to formats subdir
    While i dont plan on implimenting every chunk type; seperating the common from the uncommon is a good first step;

--- src/packer/asura/models/chunks/__init__.py
+++ src/packer/asura/models/chunks/__init__.py
@@ -1,19 +1,14 @@
 __all__ = [
     "BaseChunk",
-    "FontInfoChunk",
     "ChunkHeader",
-    "HTextChunk",
-    "HText",
-    "ResourceDescription",
     "RawChunk",
-    "ResourceChunk",
-    "ResourceListChunk",
-    "SoundChunk",
-    "SoundClip",
     "UnparsedChunk",
-    "EofChunk"
+    "EofChunk",
+    "formats"
 ]
-from packer.asura.models.chunks.header import ChunkHeader # FIRST IMPORT ALWAYS
+
+# FIRST IMPORT ALWAYS
+from packer.asura.models.chunks.header import ChunkHeader
 # Depends on ChunkHeader ONLY
 from packer.asura.models.chunks.base import BaseChunk
 
@@ -21,11 +16,3 @@ from packer.asura.models.chunks.base import BaseChunk
 from packer.asura.models.chunks.unparsed import UnparsedChunk
 from packer.asura.models.chunks.raw import RawChunk
 from packer.asura.models.chunks.eof import EofChunk
-
-# Depends on ANY of the above
-from packer.asura.models.chunks.font_info import FontInfoChunk
-from packer.asura.models.chunks.htext import HTextChunk, HText
-from packer.asura.models.chunks.resource import ResourceChunk
-from packer.asura.models.chunks.resource_list import ResourceListChunk, ResourceDescription
-from packer.asura.models.chunks.sound import SoundChunk, SoundClip
-
